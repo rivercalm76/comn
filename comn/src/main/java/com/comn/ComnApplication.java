acdeccfd57bdc04acc5188a9064daa6491348d09
kcs <kcs@DESKTOP-IRSJ0JN>
2022-10-03 18:38:42 +0900

    init

---
+++ comn/src/main/java/com/comn/ComnApplication.java
@@ -0,0 +1,13 @@
+package com.comn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComnApplication.class, args);
+	}
+
+}
---
+++ comn/src/main/java/com/comn/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.comn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+	
+}
